47699ed2cc456ccc5e9e7f4de663778dc5137c8e
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2023-07-06 13:56:57 -0600

    gotta push
    pushisssss

--- js/script.js
+++ js/script.js
@@ -3,9 +3,14 @@ function hideDivs(){
 };
 
 function showDivs(){
-
+    var div = document.getElementById('game-page' || 'intro-page');
+    if (div.style.display === 'none') {
+      div.style.display = 'block';
+    } else {
+      div.style.display = 'none';
+    }
 }
 
 function game(){
-    
+
 }
